09ab499b421db82cb85d456dcb993dc0d081de7c
Deandra Abiantoro <deandra.abiantoro@approperti.co.id>
2023-12-08 13:15:59 +0700

    fix error login

--- resources/views/component/head.blade.php
+++ resources/views/component/head.blade.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="utf-8" />
     
-    <title> {{ $judul }}</title>
+    <title> {{ $judul ?? '' }}</title>
     <meta name="description" content="Latest updates and statistic charts"> 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
 
